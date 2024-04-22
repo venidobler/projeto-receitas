98e562ca9cd963eded41d495961ad54b204006f2
Venícius Rocha Dobler <veniciusdobler840@gmail.com>
2024-04-22 11:50:53 -0300

    feat: nova url para uma nova view

--- recipes/views.py
+++ recipes/views.py
@@ -3,5 +3,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def home (request):
-    return render (request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Luiz Otávio',
+    })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Luiz Otávio',
+    })
 
